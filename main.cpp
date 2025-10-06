fd116eb0fff2f2cdc68a9318acd09fb6a8ccc957
Dirk Zimmermann <me@dirkz.com>
2025-10-06 10:17:56 +0200

    One main.cpp with WinMain, unicode

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <windows.h>
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	OutputDebugString(L"Hello, world!\n");
+
+	// TODO: Return wParam of WM_QUIT message.
+	// https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
+	return 0;
+}
