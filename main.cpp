84a54c595131a77fdece13cf9874875d1ad0dbff
Dirk Zimmermann <me@dirkz.com>
2025-10-06 10:41:16 +0200

    Show a window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,78 @@
 ﻿#include <windows.h>
 
+#include <string>
+
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // All painting occurs here, between BeginPaint and EndPaint.
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+static int CreateWindowAndRunMessageLoop(HINSTANCE hInstance, int nShowCmd)
+{
+
+    const std::wstring WindowClassName{L"zxultra_class"};
+
+    WNDCLASS wc{};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = WindowClassName.c_str();
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(0, // optional windows styles
+                               WindowClassName.c_str(),
+                               L"zxultra",          // window title
+                               WS_OVERLAPPEDWINDOW, // window style
+                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               nullptr, // parent window
+                               nullptr, // menu
+                               hInstance,
+                               nullptr // additional application state
+    );
+
+    if (hwnd == nullptr)
+    {
+        OutputDebugString(L"Could not create window, will abort");
+        return 0;
+    }
+
+    ShowWindow(hwnd, nShowCmd);
+
+    // Run the message loop.
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return static_cast<int>(msg.wParam);
+}
+
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
     OutputDebugString(L"Hello, world!\n");
 
-    // TODO: Return wParam of WM_QUIT message.
-    // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
-    return 0;
+    return CreateWindowAndRunMessageLoop(hInstance, nShowCmd);
 }
