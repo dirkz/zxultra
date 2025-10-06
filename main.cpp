64a320c0cc76772c15e73cfff6606fc267421036
Dirk Zimmermann <me@dirkz.com>
2025-10-06 10:18:27 +0200

    Format

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 ﻿#include <windows.h>
 
-int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	OutputDebugString(L"Hello, world!\n");
+    OutputDebugString(L"Hello, world!\n");
 
-	// TODO: Return wParam of WM_QUIT message.
-	// https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
-	return 0;
+    // TODO: Return wParam of WM_QUIT message.
+    // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
+    return 0;
 }
