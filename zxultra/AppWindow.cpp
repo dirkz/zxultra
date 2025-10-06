79721cbda975cecdc969faf27941f6dff84a3c62
Dirk Zimmermann <me@dirkz.com>
2025-10-06 17:09:18 +0200

    Get a temporary factory

--- zxultra/AppWindow.cpp
+++ zxultra/AppWindow.cpp
@@ -6,11 +6,15 @@
 
 #include "Error.h"
 
+using Microsoft::WRL::ComPtr;
+
 namespace zxultra
 {
 
 void DXWindow::OnHwndCreated(HWND hwnd)
 {
+    ComPtr<IDXGIFactory1> factory;
+    ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(factory.GetAddressOf())));
 }
 
 void DXWindow::Resize(int width, int height)
