f8102fd9f930bc63dd81f97670255b45d45a9aef
Dirk Zimmermann <me@dirkz.com>
2025-10-14 14:58:24 +0200

    Possible std::hash for VertexWithColor

--- zxultra/VertexWithColor.h
+++ zxultra/VertexWithColor.h
@@ -2,11 +2,15 @@
 
 #include "stdafx.h"
 
+#include <Util.h>
+
 namespace zxultra
 {
 
 struct VertexWithColor
 {
+    friend std::hash<zxultra::VertexWithColor>;
+
     VertexWithColor(XMFLOAT3 position, FXMVECTOR color) : m_position{position}
     {
         XMStoreFloat4(&m_color, color);
@@ -18,3 +22,24 @@ struct VertexWithColor
 };
 
 } // namespace zxultra
+
+template <> struct std::hash<zxultra::VertexWithColor>
+{
+    std::size_t operator()(const zxultra::VertexWithColor &v)
+    {
+        using zxultra::hash_combine;
+
+        std::hash<float> floatHasher{};
+
+        std::size_t h = floatHasher(v.m_position.x);
+        hash_combine(h, floatHasher(v.m_position.y));
+        hash_combine(h, floatHasher(v.m_position.z));
+
+        hash_combine(h, floatHasher(v.m_color.x));
+        hash_combine(h, floatHasher(v.m_color.y));
+        hash_combine(h, floatHasher(v.m_color.z));
+        hash_combine(h, floatHasher(v.m_color.w));
+
+        return h;
+    }
+};
