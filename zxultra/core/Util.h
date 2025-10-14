aaefeb155ed8c3a3b4528442db6bc1cad6a99c76
Dirk Zimmermann <me@dirkz.com>
2025-10-14 14:41:49 +0200

    Fix warning about initalization

--- zxultra/core/Util.h
+++ zxultra/core/Util.h
@@ -7,7 +7,7 @@ namespace zxultra
 
 template <class T, class H = std::hash<T>> inline void hash_combine(std::size_t &seed, const T &v)
 {
-    H hasher;
+    H hasher{};
     seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
