86afaae16b5874ffc27951505940c8429b669a80
Dirk Zimmermann <me@dirkz.com>
2025-10-14 14:41:30 +0200

    core Util with a hash_combine

---
+++ zxultra/core/Util.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <functional>
+
+namespace zxultra
+{
+
+template <class T, class H = std::hash<T>> inline void hash_combine(std::size_t &seed, const T &v)
+{
+    H hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+} // namespace zxultra
