25fbb07edc1b2fc2efe99df488b5586ff388e4f4
Dirk Zimmermann <me@dirkz.com>
2025-10-15 11:28:08 +0200

    Basic vertex shader for position/color (VS)

---
+++ shaders/PositionColor.hlsl
@@ -0,0 +1,27 @@
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 WorldViewProjection;
+}
+
+struct Vertex
+{
+    float3 Position : POSITION;
+    float4 Color : COLOR[n];
+};
+
+struct Fragment
+{
+    float4 Position : SV_Position;
+    float4 Color : COLOR;
+};
+
+Fragment VS(Vertex v)
+{
+    Fragment fragment;
+
+    float4 hPosition = float4(v.Position, 1);
+    fragment.Position = mul(hPosition, WorldViewProjection);
+    fragment.Color = v.Color;
+    
+    return fragment;
+}
