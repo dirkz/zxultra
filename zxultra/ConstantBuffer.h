39cad71a9af8d773257b7e6186770a3b0e7f6697
Dirk Zimmermann <me@dirkz.com>
2025-10-29 18:54:31 +0100

    ConstantBuffer move constructor

--- zxultra/ConstantBuffer.h
+++ zxultra/ConstantBuffer.h
@@ -36,6 +36,12 @@ template <class T> struct ConstantBuffer
 
     ConstantBuffer &operator=(const ConstantBuffer &) = delete;
 
+    ConstantBuffer(ConstantBuffer &&other)
+        : m_numElements{std::move(other.m_numElements)}, m_resource{std::move(other.m_resource)},
+          m_mappedBuffer{std::move(other.m_mappedBuffer)}
+    {
+    }
+
     ConstantBuffer &operator=(ConstantBuffer &&other) noexcept
     {
         std::swap(m_numElements, other.m_numElements);
@@ -54,7 +60,7 @@ template <class T> struct ConstantBuffer
         }
     }
 
-    inline void Swap(ConstantBuffer<T>& other)
+    inline void Swap(ConstantBuffer<T> &other)
     {
         *this = std::move(other);
     }
