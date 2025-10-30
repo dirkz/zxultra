a5a1d58aa700087ce4da7ed4fc6b2a50bfd670ef
Dirk Zimmermann <me@dirkz.com>
2025-10-30 12:13:39 +0100

    Create a second model matrix for the right cube (incoming)

--- zxultra/AppWindow.cpp
+++ zxultra/AppWindow.cpp
@@ -86,8 +86,15 @@ void AppWindow::Update(double elapsedSeconds)
     XMMATRIX modelLeft = XMMatrixMultiply(model1, model2);
     modelLeft = XMMatrixMultiply(modelLeft, translateLeft);
 
+    model1 = XMMatrixRotationY(-rotationY);
+    model2 = XMMatrixRotationX(-rotationX);
+    XMMATRIX translateRight = XMMatrixTranslation(+1, 0, 0);
+    XMMATRIX modelRight = XMMatrixMultiply(model1, model2);
+    modelRight = XMMatrixMultiply(modelRight, translateRight);
+
     XMStoreFloat4x4(&frameData.PerPass(), XMMatrixTranspose(viewProjection));
     XMStoreFloat4x4(&frameData.PerObject(0), XMMatrixTranspose(modelLeft));
+    XMStoreFloat4x4(&frameData.PerObject(1), XMMatrixTranspose(modelRight));
 }
 
 void AppWindow::Draw()
